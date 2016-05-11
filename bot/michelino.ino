215fae14221684ad11aa1613fec8023222cf08e7
AbhedyaK <Kabhedya19@gmail.com>
2016-05-11 17:02:29 +0530

    Bluetooth Controlled Bot

---
+++ bot/michelino.ino
@@ -0,0 +1,206 @@
+// bot
+// ©Abhedya Khatiwala
+
+
+#define LOGGING
+
+// Device drivers
+// Enable one driver in each category
+
+// Motor controller:
+#define ENABLE_ADAFRUIT_MOTOR_DRIVER
+
+// Distance sensor
+#define ENABLE_NEWPING_DISTANCE_SENSOR_DRIVER
+
+// Remote control:
+#define ENABLE_BLUESTICK_REMOTE_CONTROL_DRIVER
+//#define ENABLE_ROCKETBOT_REMOTE_CONTROL_DRIVER
+
+// constants
+#define TOO_CLOSE 20                    /**< distance to obstacle in centimeters */
+#define MAX_DISTANCE (TOO_CLOSE * 20)   /**< maximum distance to track with sensor */
+#define RANDOM_ANALOG_PIN 5             /**< unused analog pin to use as random seed */
+#define BT_RX_PIN 16                    /**< RX pin for Bluetooth communcation */
+#define BT_TX_PIN 17                    /**< TX pin for Bluetooth communcation */
+
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(BT_RX_PIN, BT_TX_PIN);
+
+#ifdef ENABLE_ADAFRUIT_MOTOR_DRIVER
+#include <AFMotor.h>
+#include "adafruit_motor_driver.h"
+#define LEFT_MOTOR_INIT 1
+#define RIGHT_MOTOR_INIT 3
+#endif
+
+#ifdef ENABLE_NEWPING_DISTANCE_SENSOR_DRIVER
+#include <NewPing.h>
+#include "newping_distance_sensor.h"
+#define DISTANCE_SENSOR_INIT 14,15,MAX_DISTANCE
+#endif
+
+#ifdef ENABLE_BLUESTICK_REMOTE_CONTROL_DRIVER
+//#include "bluestick_remote_control.h"
+#include "m_bluestick_remote_control.h"
+#define REMOTE_CONTROL_INIT
+#endif
+
+#
+
+#include "logging.h"
+#include "moving_average.h"
+
+namespace Michelino
+{
+    class Robot
+    {
+    public:
+        /*
+         * @brief Class constructor.
+         */
+        Robot()
+            : leftMotor(LEFT_MOTOR_INIT), rightMotor(RIGHT_MOTOR_INIT),
+              distanceSensor(DISTANCE_SENSOR_INIT),
+              distanceAverage(TOO_CLOSE * 10),
+              remoteControl(REMOTE_CONTROL_INIT)
+        {
+            initialize();
+        }
+        
+        /*
+         * @brief Initialize the robot state.
+         */
+        void initialize()
+        {
+            randomSeed(analogRead(RANDOM_ANALOG_PIN));
+            remote();
+        }
+        
+        /*
+         * @brief Update the state of the robot based on input from sensor and remote control.
+         *  Must be called repeatedly while the robot is in operation.
+         */
+        void run()
+        {
+            unsigned long currentTime = millis();
+            int distance = distanceAverage.add(distanceSensor.getDistance());
+            RemoteControlDriver::command_t remoteCmd;
+            bool haveRemoteCmd = remoteControl.getRemoteCommand(remoteCmd);
+            log("state: %d, currentTime: %lu, distance: %u remote: (%d,l:%d,r:%d,k:%d)\n", 
+                state, currentTime, distance, 
+                haveRemoteCmd, remoteCmd.left, remoteCmd.right, remoteCmd.key);
+            
+            if (remoteControlled()) {
+                if (haveRemoteCmd) {
+                    switch (remoteCmd.key) {
+                    case RemoteControlDriver::command_t::keyF1:
+                        // start "roomba" mode
+                        move();
+                        break;
+                    case RemoteControlDriver::command_t::keyNone:
+                        // this is a directional command
+                        leftMotor.setSpeed(remoteCmd.left);
+                        rightMotor.setSpeed(remoteCmd.right);
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+            else {
+                // "roomba" mode
+                if (haveRemoteCmd && remoteCmd.key == RemoteControlDriver::command_t::keyF1) {
+                    // switch back to remote mode
+                    remote();
+                }
+                else {
+                    if (moving()) {
+                        if (obstacleAhead(distance))
+                            turn(currentTime);
+                    }
+                    else if (turning()) {
+                        if (doneTurning(currentTime, distance))
+                            move();
+                    }
+                }
+            }
+        }
+
+    protected:
+        void remote()
+        {
+            leftMotor.setSpeed(0);
+            rightMotor.setSpeed(0);
+            state = stateRemote;
+        }
+        
+        void move()
+        {
+            leftMotor.setSpeed(-255);
+            rightMotor.setSpeed(-255);
+            state = stateMoving;
+        }
+        
+        void stop()
+        {
+            leftMotor.setSpeed(0);
+            rightMotor.setSpeed(0);
+            state = stateStopped;
+        }
+        
+        bool obstacleAhead(unsigned int distance)
+        {
+            return (distance <= TOO_CLOSE);
+        }
+        
+        bool turn(unsigned long currentTime)
+        {
+            if (random(2) == 0) {
+                leftMotor.setSpeed(255);
+                rightMotor.setSpeed(-255);
+            }
+            else {
+                leftMotor.setSpeed(-255);
+                rightMotor.setSpeed(255);
+            }
+            state = stateTurning;
+            endStateTime = currentTime + random(500, 1000);
+        }
+        
+        bool doneTurning(unsigned long currentTime, unsigned int distance)
+        {
+            if (currentTime >= endStateTime)
+                return (distance > TOO_CLOSE);
+            return false;
+        }
+        
+        bool moving() { return (state == stateMoving); }
+        bool turning() { return (state == stateTurning); }
+        bool stopped() { return (state == stateStopped); }
+        bool remoteControlled() { return (state == stateRemote); }
+
+    private:
+        Motor leftMotor;
+        Motor rightMotor;
+        DistanceSensor distanceSensor;
+        MovingAverage<unsigned int, 3> distanceAverage;
+        RemoteControl remoteControl;
+        enum state_t { stateStopped, stateMoving, stateTurning, stateRemote };
+        state_t state;
+        unsigned long endStateTime;
+    };
+};
+
+Michelino::Robot robot;
+
+void setup()
+{
+    Serial.begin(9600);
+    BTSerial.begin(9600);
+}
+
+void loop()
+{
+    robot.run();
+}
---
+++ motor_driver.h
@@ -0,0 +1,23 @@
+
+
+namespace Michelino
+{
+    class MotorDriver
+    {
+    public:
+        /**
+         * @brief Change the speed of the motor.
+         * @param speed The new speed of the motor.
+         *  Valid values are between -255 and 255. 
+         *  Use positive values to run the motor forward, 
+         *  negative values to run it backward and zero to stop the motor.
+         */
+        virtual void setSpeed(int speed) = 0;
+        
+        /**
+         * @brief Return the current speed of the motor.
+         * @return The current speed of the motor with range -255 to 255.
+         */
+        virtual int getSpeed() const = 0;            
+    };
+};
